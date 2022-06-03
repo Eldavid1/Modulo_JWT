93a060e374d5fc3ba4de4d7af5fe2bcde8086ede
DiegoOrtegaFranco <201611285@tese.edu.mx>
2022-06-02 20:11:16 -0500

    se agrega controller de usuarios

---
+++ src/main/java/com/netjstech/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.netjstech.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping("/user")
+public class UserController {
+	@GetMapping("/allusers")
+	public String displayUsers() {
+		return "Display All Users";
+	}
+	
+}
